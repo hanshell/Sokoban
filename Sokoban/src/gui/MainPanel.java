4f532fcad34c3b7980bfe5ed70f4d0eb2c72d3c6
hanshell <hanshmelby@gmail.com>
2013-10-12 17:39:58 +0200

    Created main JPanel with GridLayout

--- Sokoban/src/gui/MainPanel.java
+++ Sokoban/src/gui/MainPanel.java
@@ -1,7 +1,26 @@
 package gui;
 
+import graphics.Map;
+
+import java.awt.BorderLayout;
+import java.io.IOException;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainPanel extends JPanel {
+	public MainPanel() throws IOException{
+		setLayout(new BorderLayout());
+		add(new TilePanel(new Map("C:\\Users\\Melbster\\git\\Sokoban\\Sokoban\\src\\graphics\\test.txt")));
+	}
+	public static void main(String[] args) throws IOException {
+		JFrame frame=new JFrame();
+		frame.getContentPane().add(new MainPanel());
+		frame.setResizable(true);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
 	
 }
