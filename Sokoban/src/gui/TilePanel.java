477822387b3bbc5963712370a3b0957b967ba8e1
hanshell <hanshmelby@gmail.com>
2013-10-12 17:42:20 +0200

    Cleaned up messy commentary

--- Sokoban/src/gui/TilePanel.java
+++ Sokoban/src/gui/TilePanel.java
@@ -3,11 +3,35 @@ package gui;
 import graphics.Map;
 
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class TilePanel extends JPanel{	
+import tiles.OpenSpaceTile;
+
+public class TilePanel extends JPanel{
 	public TilePanel(Map map){
-		setLayout(new GridLayout(map.getLines().size(), map.getLines().get(0).size()));
+		setLayout(new GridLayout(map.getTiles().size(), map.getTiles().get(0).size()));
 		
+		for(int i=0; i<map.getTiles().size(); i++){
+			for(int j=0; j<map.getTiles().get(0).size(); j++){
+				BufferedImage img=null;
+				try{
+					img=ImageIO.read(new File(Map.tileType(map.getTiles().get(i).get(j).getCharacter()).getFilePath()));
+				}
+				catch (Exception e) {
+					System.err.println("Error: Couldn't read image");
+					// TODO: handle exception
+				}
+				Image resizedImage=img.getScaledInstance(32, 32, Image.SCALE_DEFAULT);
+				
+				this.add(new JLabel(new ImageIcon(resizedImage)));
+			}
+		}
 	}
 }
