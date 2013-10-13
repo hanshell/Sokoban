8c0c401262e4cea3e8ba7464f80b8f35b17e8782
hanshell <hanshmelby@gmail.com>
2013-10-13 19:19:18 +0200

    TilePanel is now updated dynamically

--- Sokoban/src/gui/MainPanel.java
+++ Sokoban/src/gui/MainPanel.java
@@ -3,24 +3,36 @@ package gui;
 import graphics.Map;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import tiles.BoardTile;
+
+import logic.Logic;
+
 public class MainPanel extends JPanel {
+	private ArrayList<ArrayList<BoardTile>> tiles;
+	private TilePanel tilepanel;
 	public MainPanel() throws IOException{
 		setLayout(new BorderLayout());
-		add(new TilePanel(new Map("C:\\Users\\Melbster\\git\\Sokoban\\Sokoban\\src\\graphics\\test.txt")));
+		tilepanel=new TilePanel(new Map("C:\\Users\\Melbster\\git\\Sokoban\\Sokoban\\src\\graphics\\test.txt").getTiles());
+		tilepanel.setFocusable(true);
+		add(tilepanel);
 	}
 	public static void main(String[] args) throws IOException {
 		JFrame frame=new JFrame();
 		frame.getContentPane().add(new MainPanel());
-		frame.setResizable(true);
+		frame.setResizable(false);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		frame.setVisible(true);	
 	}
-	
 }
--- Sokoban/src/gui/TilePanel.java
+++ Sokoban/src/gui/TilePanel.java
@@ -4,28 +4,46 @@ import graphics.Map;
 
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import logic.Logic;
+
+import tiles.BoardTile;
 import tiles.OpenSpaceTile;
 
 public class TilePanel extends JPanel{
-	public TilePanel(Map map){
-		setLayout(new GridLayout(map.getTiles().size(), map.getTiles().get(0).size()));
+	private ArrayList<ArrayList<BoardTile>> tiles;
+	private Logic logic;
+	
+	public TilePanel(ArrayList<ArrayList<BoardTile>> tiles){
+		setLayout(new GridLayout(tiles.size(), tiles.get(0).size()));
+
+		this.tiles=tiles;
+		paint(tiles);
 		
-		for(int i=0; i<map.getTiles().size(); i++){
-			for(int j=0; j<map.getTiles().get(0).size(); j++){
+		logic=new Logic(tiles);
+		
+		this.addKeyListener(new MovePlayer());
+	}
+	public void paint(ArrayList<ArrayList<BoardTile>> tiles){
+		
+		for(int i=0; i<tiles.size(); i++){
+			for(int j=0; j<tiles.get(0).size(); j++){
 				BufferedImage img=null;
 				try{
-					img=ImageIO.read(new File(Map.tileType(map.getTiles().get(i).get(j).getCharacter()).getFilePath()));
+					img=ImageIO.read(new File(tiles.get(i).get(j).getFilePath()));
 				}
 				catch (Exception e) {
-					System.err.println("Error: Couldn't read image");
+					System.err.println("Error: Couldn't read image "+tiles.get(i).get(j).getCharacter());
 					// TODO: handle exception
 				}
 				Image resizedImage=img.getScaledInstance(32, 32, Image.SCALE_DEFAULT);
@@ -33,5 +51,40 @@ public class TilePanel extends JPanel{
 				this.add(new JLabel(new ImageIcon(resizedImage)));
 			}
 		}
+	}
+	public void printTiles(){
+		for(int i=0; i<tiles.size(); i++){
+			for(int j=0; j<tiles.get(i).size(); j++){
+				System.out.print(tiles.get(i).get(j).getCharacter());
+			}
+			System.out.println();
+		}
+		System.out.println("\n\n");
+	}
+	private class MovePlayer implements KeyListener{
+		@Override
+		public void keyPressed(KeyEvent arg0) {
+			if(arg0.getKeyCode()==KeyEvent.VK_UP)
+				logic.moveUp();
+			if(arg0.getKeyCode()==KeyEvent.VK_DOWN)
+				logic.moveDown();
+			if(arg0.getKeyCode()==KeyEvent.VK_LEFT)
+				logic.moveLeft();
+			if(arg0.getKeyCode()==KeyEvent.VK_RIGHT)
+				logic.moveRight();
+			removeAll();
+			paint(tiles);
+			revalidate();
+		}
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+
+		}
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+
+			// TODO Auto-generated method stub
+		}
+		
 	}
 }
